0316517ae80866e0e8e5429a7051b16f29627e31
Jorge Blanco <jbsfreelance@gmail.com>
2019-11-14 22:26:45 -0800

    Added a player scaler script to handle the different player size phases

---
+++ Assets/Scripts/PlayerScaler.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using Zinnia.Data.Type;
+
+public class PlayerScaler : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+
+    public void ScalePlayer(float scale)
+    {
+        player.localScale = new Vector3(scale, scale, scale);
+        // player.SetGlobalScale(new Vector3(scale, scale, scale));
+        Debug.Log($"Tried to scale to {scale}");
+    }
+
+    public void ScalePlayerByTransform(TransformData target)
+    {
+        ScalePlayer(target.Transform.localScale.y);
+    }
+}
