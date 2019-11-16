784cc8e21ebff411d65389a5eadb7f1fc6a94fdf
Jorge Blanco <jbsfreelance@gmail.com>
2019-11-15 23:27:16 -0800

    Added player scaling targets

---
+++ Assets/Scripts/PlayerScalePoint.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using VRTK.Prefabs.Locomotion.Teleporters;
+using Zinnia.Data.Type;
+
+public class PlayerScalePoint : MonoBehaviour
+{
+    [SerializeField] private PlayerScaler playerScaler;
+    [SerializeField] private TeleporterFacade teleporter;
+    private Transform _scaledTarget;
+    private SizePointActivator _sizePointActivator;
+
+    private void Start()
+    {
+        _scaledTarget = GetComponent<Transform>();
+        _sizePointActivator = GetComponentInParent<SizePointActivator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        playerScaler.ScalePlayer(_scaledTarget.localScale.y);
+        teleporter.Teleport(new TransformData(_scaledTarget.transform));
+        _sizePointActivator.ActivatePoints(excluded:gameObject);
+    }
+}
---
+++ Assets/Scripts/SizePointActivator.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class SizePointActivator : MonoBehaviour
+{
+    public void ActivatePoints(GameObject excluded)
+    {
+        foreach (Transform child in transform)
+        {
+            if (child.gameObject == excluded)
+            {
+                child.gameObject.SetActive(false);
+            }
+            else
+            {
+                child.gameObject.SetActive(true);
+            }
+        }
+    }
+}
