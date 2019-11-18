3b8e90e24e6d05827f70fa28230b19d8b36e2da5
Jorge Blanco <jbsfreelance@gmail.com>
2019-11-17 23:44:25 -0800

    Added a simple display that changes output with the in-game button presses

---
+++ Assets/Scripts/ShapeDisplay.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class ShapeDisplay : MonoBehaviour
+{
+    [SerializeField] private Renderer display;
+    [SerializeField] private Material[] shapes;
+
+    private int _currentShapeIndex;
+    private int _shapeCount;
+
+    private void Start()
+    {
+        _shapeCount = shapes.Length;
+    }
+
+    public void NextShape()
+    {
+        _currentShapeIndex = (_currentShapeIndex + 1) % _shapeCount;
+        display.sharedMaterial = shapes[_currentShapeIndex];
+    }
+}
